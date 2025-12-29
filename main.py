d6d5d362f011251a2c621244348166dc1456370f
Micstive <micahstivers@gmail.com>
2025-12-29 08:48:21 -0800

    bookbot added capability to receive a book to scan from the comand line

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_num_chars, get_sorted
+import sys
 
 def get_book_text(filepath):
     contents = ""
@@ -7,10 +8,13 @@ def get_book_text(filepath):
     return contents
 
 def main():
-    contents = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    contents = get_book_text(sys.argv[1])
     number_of_words = get_num_words(contents)
     number_of_different_chars = get_num_chars(contents)
-    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\nFound {number_of_words} total words\n--------- Character Count -------")
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {sys.argv[1]}...\n----------- Word Count ----------\nFound {number_of_words} total words\n--------- Character Count -------")
     char_key = "char"
     num_key = "num"
     for char_dict in get_sorted(number_of_different_chars):
